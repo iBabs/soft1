7c87914b4e35af0ccdb1d1b2ca62db65770fbe0d
iBabs <ibrahimbabalola8@gmail.com>
2024-03-05 16:23:51 +0100

    new files add

---
+++ script.js
@@ -0,0 +1,68 @@
+//alert("hi there")
+
+// number
+// 0.05 is float
+// 2 is an int
+
+//string
+
+"Hello" 
+"1000"
+
+//boolean
+//true or false
+
+
+
+
+
+let up = 10
+var down = 15
+
+
+console.log(up + down)
+
+let firstName = "Yomi"
+const last_name = "Ajagbandi"
+
+let fullname = firstName + " "+ last_name
+
+
+console.log(fullname)
+
+up = 20
+console.log("up is now:", up)
+
+// last_name= "new name"
+
+// const happy = confirm("Are you happy?")
+
+// if(happy){
+//     alert("we are happy")
+// }else{
+//     alert("Baba please don't cry")
+// }
+
+const password = prompt("enter pin")
+
+let balance = 5000
+
+// const withdrawal = parseInt(prompt("How much would you like to withdraw"))
+
+// if(password=="1234" && balance>=withdrawal){
+//     alert('You have successfully withdrawn '+ withdrawal+"\n Your new balance is " + (balance-withdrawal))
+// } else{
+//     alert("you broke die o lmao! or you sabi your pin again")
+// }
+
+if(password=="1234"){
+    alert("welcome to wayo bank")
+    const withdrawal = parseInt(prompt("How much would you like to withdraw"))
+    if(balance>=withdrawal){
+        alert('You have successfully withdrawn '+ withdrawal+"\n Your new balance is " + (balance-withdrawal))
+        } else{
+            alert("you broke die o lmao! or you sabi your pin again")
+        }
+}else{
+    alert("you nio sabi password abi you wan tif")
+}
